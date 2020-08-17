5fa6b0fa75652ff83330b28cebc10b9d64a09c86
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-08-17 15:08:25 +0200

    inlines_header_introduced

---
+++ lib/include/processing_inlines.hpp
@@ -0,0 +1,15 @@
+// processing_inlines.hpp
+#pragma once
+#ifndef PROCESSING_INLINES_H
+#define PROCESSING_INLINES_H
+#include <cmath>
+//using namespace std::math;
+///
+///
+///
+namespace Processing
+{
+
+
+}//END of namespace Processing
+#endif
