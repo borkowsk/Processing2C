b88912acff6a91cb8fcc86f73cf6636e3e2c8434
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-08-17 23:07:12 +0200

    Translation_products_20200817

--- PROJECTS/RTSI2020s/cppsrc/UtilCharts.pde.cpp
+++ PROJECTS/RTSI2020s/cppsrc/UtilCharts.pde.cpp
@@ -1,9 +1,10 @@
 //Processing to C++ converter ../../scripts/procesing2cpp.sh
 //Source: UtilCharts.pde
 #include "processing_window.hpp"
+#include "processing_templates.hpp"
 #include "processing_library.hpp"
 #include "processing_console.hpp" //is optional. Should be deleted when not needed
-//include "processing_inlines.hpp" //is optional. Use when project is already compilable
+#include "processing_inlines.hpp" //is optional. Use when project is already compilable
 using namespace Processing;
 #include "local.h"
 #include "project.h" //Is's for you. Could be deleted when not needed
