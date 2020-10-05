e65fc72585217a48287e71dd53dfc4188bfb12c9
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-10-05 17:49:27 +0200

    jakby_działa

--- lib/include/symbol_lst.sh
+++ lib/include/symbol_lst.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 #egrep "^([^#^/\s]*)([A-Za-z][A-Za-z0-9_]*)(\s+)([A-Za-z][A-Za-z0-9_]*)" *.h* ???
-egrep '^([^#]\s)*(const|inline)*(\s+)(\w+\s+)+([A-Za-z][_A-Za-z0-1]*)([<(,])' *.hpp
+egrep -h '^([^#]\s)*(friend|virtual|const|inline)*(\s+)(\w+\s+)+([A-Za-z][_A-Za-z0-1]*)([<(,])' *.hpp |\
+sed -E 's@(\s*)((friend|virtual|const|inline)\s*)*(.*)$@\4@g' |\
+sed -E 's@(auto|bool|void|int|size_t|long|float|double|String)(\s*)([A-Za-z][_A-Za-z0-1]*)([<(,])(.*)$@\3@'|\
+sort | uniq
