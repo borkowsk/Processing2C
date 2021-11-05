3214f14a5f3e844c51e90d2a63a464fb2683909c
Wojciech T. Borkowski <wborkowsk@gmail.com>
2021-11-05 16:35:24 +0100

    sHashMap<>_Introduced

--- lib/include/processing_map.hpp
+++ lib/include/processing_map.hpp
@@ -17,9 +17,26 @@ class HashMap:public std::map<T,X>
 };
 
 
+template<class T,class X>
+class sHashMap:public self_printable_ptr< HashMap<T,X> >
+{
+    ///INFO: Processing semantics for hash map
+public:
+    ~sHashMap(){}// Zwalnianie zasobów
+    sHashMap(){}
+    //sArrayList(std::initializer_list<T> lst);
+    sHashMap(HashMap<T,X>* tab): self_printable_ptr< HashMap<T,X> >(tab){}
+    HashMap<T,X>* operator -> () { return this->get();}
+    auto  begin() { return this->get()->begin(); } //need C++14 !
+    auto  end() { return this->get()->end(); } //need C++14 !
+    int 	size() const //Returns the number of elements in this list.
+    { return this->get()->size(); }
+};
+
+
 }//END of namespace Processing
 /********************************************************************/
-/*               PROCESSING2C  version 2020-08-19                   */
+/*               PROCESSING2C  version 2021-11-05                   */
 /********************************************************************/
 /*           THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
 /*            W O J C I E C H   B O R K O W S K I                   */
