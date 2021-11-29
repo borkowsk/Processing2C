0b3b03b6e22261a29e76b9eed779def2b21fd4cf
Wojciech T. Borkowski <wborkowsk@gmail.com>
2021-11-29 14:35:16 +0100

    STRINGTEST added

---
+++ PROJECTS/STRINGTEST/STRINGTEST.pde
@@ -0,0 +1,5 @@
+/// String operations test
+/// Author: Wojciech Borkowski wborkowski_uw_edu_pl
+/// ORIGINAL FILE: "STRINGTEST.PDE"
+/////////////////////////////////////////////////////////////////
+
---
+++ PROJECTS/STRINGTEST/cppsrc/exit.pde.cpp
@@ -0,0 +1,23 @@
+//Processing to C++ converter ../../scripts/procesing2cpp.sh
+//Source: exit.pde
+#include "processing_consts.hpp"
+#include "processing_templates.hpp"
+#include "processing_library.hpp"
+#include "processing_window.hpp"
+//#include "processing_inlines.hpp" //...is optional. Use when project is already compilable!
+#include "processing_console.hpp"   //...is optional. Should be deleted when not needed.
+#include "processing_alist.hpp" //...is optional. Should be deleted when not needed.
+#include "processing_lists.hpp" //...is optional. Should be deleted when not needed.
+#include "processing_map.hpp"   //...is optional. Should be deleted when not needed.
+#include "processing_files.hpp" //...is optional. Should be deleted when not needed.
+#include "project.h" //...is for you. Could be deleted when not needed.
+using namespace Processing;
+#include "local.h"
+//==================================================================================
+
+void processing_window::exit()
+{
+  save("exit.png");
+}
+//../../scripts/procesing2cpp.sh did it
+
