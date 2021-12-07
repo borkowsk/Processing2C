2cc0fd2d634567d52bed4fd9e7ffdfcbdc195b11
Wojciech T. Borkowski <wborkowsk@gmail.com>
2021-12-07 23:37:21 +0100

    Rather_big_reimplementation.Now_passing_as_parameters_allowed

--- lib/include/processing_files.hpp
+++ lib/include/processing_files.hpp
@@ -13,103 +13,193 @@
 
 namespace Processing
 {
-  class BufferedReader
+  // An alternative to the currently used method of creating a reference to a type that mimics the JAVA class
+  /// A class that adds methods that are not in std::ifstream but are in a JAVA class that we are faking
+  /// See https://docs.oracle.com/javase/8/docs/api/java/io/BufferedReader.html for method details
+  class _Jifstream:public std::ifstream
   {
-   ///INFO:
-      std::ifstream* ptr;
+  public:
+      /// Tells whether this stream is ready to be read.
+      /// A buffered character stream is ready if the buffer is not empty,
+      /// or if the underlying character stream is ready.
+      bool	ready() { return is_open() && peek() != EOF;}
+
+      /// Reads a line of text.
+      String readLine();
+
+      /// Reads a single character.
+      //int	read()
+
+      ///Reads characters into a portion of an array.
+      //int	read(char[] cbuf, int off, int len)
+
+      ///Returns a Stream, the elements of which are lines read from this BufferedReader.
+      //Stream<String>	lines();
+
+      /// Tells whether this stream supports the mark() operation, which it does.
+      bool	markSupported() { return false; }
+
+      /// Marks the present position in the stream.
+      //void	mark(int readAheadLimit);
+
+      /// Resets the stream to the most recent mark.
+      //void	reset()
+
+      /// Skips characters.
+      //long	skip(long n)
+  };
+
+  /// A class that adds methods that are not in std::ofstream but are in a JAVA class that we are faking
+  /// See https://docs.oracle.com/javase/7/docs/api/java/io/PrintWriter.html for method details
+  //class _Jofstream:public std::ofstream
+  //{
+  //};
+
+  class BufferedReader: public  ptr<_Jifstream>
+  {
    public:
-      virtual ~BufferedReader();// zamknięcie i zwalnianie zasobów
+      /// Constructors
+      BufferedReader():  ptr<_Jifstream>(nullptr)
+      {}
+      BufferedReader(const std::nullptr_t&): BufferedReader()
+      {}
+      BufferedReader(_Jifstream* p):ptr<_Jifstream>(p)
+      {}
+      BufferedReader(BufferedReader& w):ptr<_Jifstream>(w)
+      {}
+      BufferedReader(const BufferedReader& w):ptr<_Jifstream>(w)
+      {}
+      /// Destructor
+      virtual ~BufferedReader() = default;
 
-      //Konstruktory
-      BufferedReader(){ptr=nullptr;}
-      BufferedReader(const std::nullptr_t&): BufferedReader(){}
-      BufferedReader(std::ifstream* p) { ptr=p;}
-      BufferedReader(BufferedReader& );
-      BufferedReader(const BufferedReader& );//need for nullptr initialisation  BUT NOT USED?
+      /// Default conversion
+      operator _Jifstream& () { return *this->_get();}
 
-      operator std::ifstream& () { return *_get();}
-
-      BufferedReader& operator = (std::ifstream* p) { ptr=p;return *this;}
-      BufferedReader& operator = (BufferedReader& );
-      BufferedReader* operator -> () { return this; }
-
-      bool operator == (const std::nullptr_t&) const { return ptr==nullptr; }
-      bool operator != (const std::nullptr_t&) const { return ptr!=nullptr; }
+      //??? is it really needed?
+      void close() { if(_ok()) this->_get()->close(); }
+      //BufferedReader& operator = (_Jifstream* p) { ptr=p;return *this;}
+      //BufferedReader& operator = (BufferedReader& );
+      //BufferedReader* operator -> () { return this; }
+      //bool operator == (const std::nullptr_t&) const { return ptr==nullptr; }
+      //bool operator != (const std::nullptr_t&) const { return ptr!=nullptr; }
+      //bool operator == (const std::nullptr_t&) const { return ptr==nullptr; }
+      //bool operator != (const std::nullptr_t&) const { return ptr!=nullptr; }
 
       String readLine();
 
-      void close() { if(ptr!=nullptr) ptr->close(); }
+      friend BufferedReader& createReader(_string_param _name);
 
-      //Not for everyday use
-      void _set(std::ifstream* p)  { ptr=p;} //for createReader ONLY
-      bool _ok() const { return ptr!=nullptr && ptr->is_open();}
-      std::ifstream* _get() const;
+  //Not for outside use
+  protected:
+      bool _ok() const
+      {
+          auto pom=_get();
+          return  pom != nullptr
+                  && pom->is_open();
+      }
+      _Jifstream* _get() const
+      {
+          return this->ptr<_Jifstream>::get();
+      }
+      void _set(std::ifstream* p); //for createReader ONLY
   };
 
+  class PrintWriter:public ptr<std::ofstream>
+  {
+   public:
+      /// Constructors
+      PrintWriter(): ptr<std::ofstream>(nullptr)
+      {}
+      PrintWriter(const std::nullptr_t&): PrintWriter()
+      {}
+      PrintWriter(std::ofstream* p): ptr<std::ofstream>(p)
+      {}
+      PrintWriter(PrintWriter& w):ptr<std::ofstream>(w)
+      {}
+      PrintWriter(const PrintWriter& w):ptr<std::ofstream>(w)
+      {}
+
+      /// Destructor - // close the stream and release resources
+      virtual ~PrintWriter()
+      {}
+
+      /// Default conversion
+      operator std::ofstream& () {return *this->_get();}
+
+      //??? is it really needed?
+      //void flush() { if(_ok()) this->_get()->flush(); }
+      //void close() { if(_ok()) this->_get()->close(); }
+      //PrintWriter&   operator = (std::ofstream* p) { ptr=p; return *this; }
+      //PrintWriter&   operator = (PrintWriter& );
+      //PrintWriter*   operator -> () { return this; }
+      //bool operator == (const std::nullptr_t&) const { return ptr==nullptr; }
+      //bool operator != (const std::nullptr_t&) const { return ptr!=nullptr; }
+
+      friend PrintWriter& createWriter(_string_param _name);
+
+  //Not for outside use
+  protected:
+      bool _ok() const
+      {
+          auto pom=_get();
+          return  pom != nullptr
+                  && pom->is_open();
+      }
+      std::ofstream* _get() const
+      {
+          return this->ptr<std::ofstream>::get();
+      }
+      void _set(std::ofstream* p); //for createWriter ONLY
+  };
+
+  /// Create opaque std::ifstream connected to file 'name'
   BufferedReader& createReader(_string_param _name);
 
-  inline
-  String BufferedReader::readLine()
-  {
-      if(ptr!=nullptr)
-      {
-          String line;
-          std::getline(*ptr, line._std_str());
-          return line;
-      }
-      else return String(nullptr);
-  }
-
-  class PrintWriter
-  {
-   ///INFO:
-      std::ofstream* ptr;
-   public:
-      virtual ~PrintWriter();// zamknięcie i zwalnianie zasobów
-
-      //Konstruktory
-      PrintWriter() {ptr=nullptr;}
-      PrintWriter(const std::nullptr_t&): PrintWriter(){}
-      PrintWriter(std::ofstream* p) { ptr=p;}
-      PrintWriter(PrintWriter& );
-      PrintWriter(const PrintWriter& );//need for nullptr initialisation BUT NOT USED?
-
-      operator std::ofstream& () {return *_get();}
-
-      PrintWriter&   operator = (std::ofstream* p) { ptr=p; return *this; }
-      PrintWriter&   operator = (PrintWriter& );
-      PrintWriter*   operator -> () { return this; }
-
-      bool operator == (const std::nullptr_t&) const { return ptr==nullptr; }
-      bool operator != (const std::nullptr_t&) const { return ptr!=nullptr; }
-
-      void flush() { if(ptr!=nullptr) ptr->flush(); }
-      void close() { if(ptr!=nullptr) ptr->close(); }
-
-      //Not for everyday use
-      bool _ok() const { return ptr!=nullptr && ptr->is_open();}
-      void _set(std::ofstream* p)  { ptr=p;} //for createWriter ONLY
-      std::ofstream* _get() const;
-  };
-
+  /// Create opaque std::ofstream connected to file 'name'
   PrintWriter& createWriter(_string_param _name);
 
-  //void print();//To bez sensu
+  /// Write something to opaqued stream
   void print(PrintWriter& o,_string_param _p1="");
-  //void print(PrintWriter& o,const String& );
-  //void print(PrintWriter& o,const std::string& );
 
+  /// Write something to opaqued stream with \n at the end
   void println(PrintWriter& o,_string_param _p1="");
-  //void println(PrintWriter& o,const String& );
-  //void println(PrintWriter& o,const std::string& );
+
+  /// Read whole line from opaqued stream (using .)
+  inline String BufferedReader::readLine()
+  {
+    if(_ok())
+    {
+        String line;
+        std::getline(*_get(), line._std_str());
+        return line;
+    }
+    else return String(nullptr);// Totally empty string
+  }
+
+  //inline String readLine(BufferedReader& r)
+  //{
+  //  return r.readLine();
+  //}
+
+  /// Read whole line from the stream (using -> )
+  inline String _Jifstream::readLine()
+  {
+      if(this->is_open())
+      {
+          String line;
+          std::getline(*this, line._std_str());
+          return line;
+      }
+      else return String(nullptr);// Totally empty string
+  }
 
 }//END of namespace Processing
 /********************************************************************/
-/*               PROCESSING2C  version 2021-10-26                   */
+/*               PROCESSING2C  version 2021-12-07                   */
 /********************************************************************/
 /*           THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
 /*            W O J C I E C H   B O R K O W S K I                   */
-/*    Instytut Studiow Spolecznych Uniwersytetu Warszawskiego       */
+/*    Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
 /*    WWW: https://www.researchgate.net/profile/WOJCIECH_BORKOWSKI  */
 /*    GITHUB: https://github.com/borkowsk                           */
 /*                                                                  */
--- lib/src/pprintwriter.cpp
+++ lib/src/pprintwriter.cpp
@@ -1,6 +1,6 @@
 // PrintWriter class
-#include "processing_consts.hpp"
-#include "processing_window.hpp"
+//#include "processing_consts.hpp"
+//#include "processing_window.hpp"
 #include "processing_templates.hpp"
 #include "processing_library.hpp"
 #include "processing_files.hpp"
@@ -18,42 +18,26 @@ static inline std::ostream& operator << (std::ostream& o,String& str)
 }
 
 /// File streams output
-PrintWriter::~PrintWriter()
+//PrintWriter::~PrintWriter()
+//{
+//    if (_ok() && (*this)->is_open()) //
+//        this->get()->close();// Czy podwójne zamknięcie jest bezpieczne?
+//}
+
+void BufferedReader::_set(std::ifstream* p)
 {
-    if(ptr!=nullptr)
-    {
-        ptr->close();
-        delete ptr;// Zwalnianie zasobów
-        ptr=nullptr;
-    }
+    // HACK!!!
+    (*this)=(_Jifstream*)p;//TODO DEBUG - looks like it works
 }
 
-// Real ;-) constructor - TRANSFERING!
-PrintWriter::PrintWriter(PrintWriter& w)
+void PrintWriter::_set(std::ofstream* p)
 {
-#ifndef NDEBUG
-    String tmp{" performed PrintWriter copy constructor "};
-    tmp+=w.ptr;
-    FIRST_TIME_ERRMESSAGE( tmp );
-#endif
-    ptr=w.ptr;w.ptr=nullptr;
-}
-
-//TRANSFERING assigment
-PrintWriter&  PrintWriter::operator = (PrintWriter& w)
-{
-#ifndef NDEBUG
-    String tmp{" performed PrintWriter operator= "};
-    tmp+=w.ptr;
-    FIRST_TIME_ERRMESSAGE( tmp );
-#endif
-    ptr=w.ptr;w.ptr=nullptr;
-    return *this;
+    (*this)=p;//TODO DEBUG - looks like it works
 }
 
 PrintWriter& createWriter(_string_param name)
 {
-    std::ofstream* file=new std::ofstream(name._std_str());             assert(file!=nullptr);
+    auto file=new std::ofstream(name._std_str());                       assert(file!=nullptr);
 
     static PrintWriter tmp;
 
@@ -63,107 +47,52 @@ PrintWriter& createWriter(_string_param name)
     }
     else
     {
-        ALWAYS_ERRMESSAGE( String("FILE ")+name+String("can't be open!") );
+        ALWAYS_ERRMESSAGE( concat("FILE ",name,"can't be open!") )
         delete file;
     }
 
     return tmp;
 }
 
-/// File streams input
-BufferedReader::~BufferedReader()
-// Zwalnianie zasobów
-{
-    if(ptr!=nullptr)
-    {
-        ptr->close();
-        delete ptr;// Zwalnianie zasobów
-        ptr=nullptr;
-    }
-}
-
-// Real ;-) constructor - TRANSFERING!
-BufferedReader::BufferedReader(BufferedReader& w)
-{
-#ifndef NDEBUG
-    String tmp{" performed BufferedReader copy constructor "};
-    tmp+=w.ptr;
-    FIRST_TIME_ERRMESSAGE( tmp );
-#endif
-    ptr=w.ptr;w.ptr=nullptr;
-}
-
-
-//TRANSFERING assigment
-BufferedReader& BufferedReader::operator = (BufferedReader& w)
-{
-#ifndef NDEBUG
-    String tmp{" performed BufferedReader operator= "};
-    tmp+=w.ptr;
-    FIRST_TIME_ERRMESSAGE( tmp );
-#endif
-    ptr=w.ptr;w.ptr=nullptr;
-    return *this;
-}
-
-
 BufferedReader& createReader(_string_param name)
 {
-    std::ifstream* file=new std::ifstream(name._std_str());                        assert(file!=nullptr);
+    auto file=new std::ifstream(name._std_str());                        assert(file!=nullptr);
 
     static BufferedReader tmp;
 
     if(file->is_open())
     {
-        file->exceptions( /*std::ifstream::failbit |*/ std::ifstream::badbit );
+        file->exceptions( /*std::ifstream::failbit | ???*/ std::ifstream::badbit );
         tmp._set(file);                                                 assert(tmp._ok());
     }
     else
     {
-        ALWAYS_ERRMESSAGE( String("FILE ")+name+String("can't be open!") );
+        ALWAYS_ERRMESSAGE( concat("FILE ",name,"can't be open!") )
         delete file;
     }
 
     return tmp;
 }
 
-std::ofstream* PrintWriter::_get() const
-{
-                                        assert(ptr!=nullptr);
-    return ptr;
-}
-
-std::ifstream* BufferedReader::_get() const
-{
-                                       assert(ptr!=nullptr);
-return ptr;
-}
-
-//void print();//To bez sensu
-//void print(PrintWriter& o,const String& ); //???
-//void print(PrintWriter& o,const std::string& ); //???
-//void println(PrintWriter& o,const String& ); //???
-//void println(PrintWriter& o,const std::string& ); //???
-
 void print(PrintWriter& o,_string_param _p1)
 {
-    *o._get()<<_p1;
+    o<<_p1;
 }
 
 void println(PrintWriter& o,_string_param _p1)
 {
-    *o._get()<<_p1<<std::endl;
+    o<<_p1<<std::endl;
 }
 
 
 }//END of namespace Processing
 
 /********************************************************************/
-/*               PROCESSING2C  version 2021-11-12                   */
+/*               PROCESSING2C  version 2021-12-07                   */
 /********************************************************************/
 /*           THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
 /*            W O J C I E C H   B O R K O W S K I                   */
-/*    Instytut Studiow Spolecznych Uniwersytetu Warszawskiego       */
+/*    Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
 /*    WWW: https://www.researchgate.net/profile/WOJCIECH_BORKOWSKI  */
 /*    GITHUB: https://github.com/borkowsk                           */
 /*                                                                  */
