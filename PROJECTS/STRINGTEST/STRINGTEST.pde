be2dfe4ec1ef917ebf73468c6793cf4652110557
Wojciech T. Borkowski <wborkowsk@gmail.com>
2021-12-14 14:24:20 +0100

    Real STRINGs example

--- PROJECTS/STRINGTEST/STRINGTEST.pde
+++ PROJECTS/STRINGTEST/STRINGTEST.pde
@@ -1,5 +1,50 @@
-/// String operations test
+/// String operations tests
 /// Author: Wojciech Borkowski wborkowski_uw_edu_pl
 /// ORIGINAL FILE: "STRINGTEST.PDE"
 /////////////////////////////////////////////////////////////////
 
+String A;                     ///
+String B=null;                ///
+String C="Ala ma kota!";      ///
+String D="😜👍 😡 🤼‍♂️ 🧐";///
+String E="Number"+1;          ///
+
+String[] SA={A,B,C,D,E};      ///
+
+StringList SL=new StringList(SA);///
+
+ArrayList<String> SAL=new ArrayList<String>();///
+
+int y=0;///
+
+void toScreen(StringList lst)  ///
+{
+  for(String s:lst)
+  if(s!=null)
+      text(s,1,y+=16);
+}
+
+void toScreen(ArrayList<String> lst)  ///
+{
+  for(String s:lst)
+  if(s!=null)
+      text(s,1,y+=16);
+}
+
+void addStrings(StringList lst1,ArrayList<String> lst2)  ///
+{
+  lst1.append(C+"? & "+E);
+  for(String s:lst1)
+    lst2.add(s);
+}
+
+void setup()  ///
+{
+  size(300,200);
+  for(String s:SA)
+    println(s);
+    
+  addStrings(SL,SAL);  
+  toScreen(SL);
+  toScreen(SAL);
+}
--- PROJECTS/STRINGTEST/cppsrc/STRINGTEST.pde.cpp
+++ PROJECTS/STRINGTEST/cppsrc/STRINGTEST.pde.cpp
@@ -1,4 +1,4 @@
-//Processing to C++ converter ../../scripts/procesing2cpp.sh
+//Processing to C++ converter /home/data/SCC/__public_git/Processing2C/scripts/procesing2cpp.sh
 //Source: STRINGTEST.pde
 #include "processing_consts.hpp"
 #include "processing_templates.hpp"
@@ -15,10 +15,57 @@ using namespace Processing;
 #include "local.h"
 //==================================================================================
 
-/// String operations test
+/// String operations tests
 /// Author: Wojciech Borkowski wborkowski_uw_edu_pl
 /// ORIGINAL FILE: "STRINGTEST.PDE"
 /////////////////////////////////////////////////////////////////
 
-//../../scripts/procesing2cpp.sh did it
+String A;                     ///
+String B=nullptr;                ///
+String C="Ala ma kota!";      ///
+String D="😜👍 😡 🤼‍♂️ 🧐";///
+String E=String("Number")+1;          ///
+
+sarray<String> SA={
+	A,B,C,D,E
+	};      ///
+
+pStringList SL=new StringList(SA);///
+
+pArrayList<String> SAL=new ArrayList<String>();///
+
+int y=0;///
+
+void toScreen(pStringList lst)  ///
+{
+  for(String s:lst)
+  if(s!=nullptr)
+      text(s,1,y+=16);
+}
+
+void toScreen(pArrayList<String> lst)  ///
+{
+  for(String s:lst)
+  if(s!=nullptr)
+      text(s,1,y+=16);
+}
+
+void addStrings(pStringList lst1,pArrayList<String> lst2)  ///
+{
+  lst1->append(C+String("? & ")+E);
+  for(String s:lst1)
+    lst2->add(s);
+}
+
+void processing_window::setup()  ///
+{
+  size(300,200);
+  for(String s:SA)
+    println(s);
+    
+  addStrings(SL,SAL);  
+  toScreen(SL);
+  toScreen(SAL);
+}
+///home/data/SCC/__public_git/Processing2C/scripts/procesing2cpp.sh did it
 
