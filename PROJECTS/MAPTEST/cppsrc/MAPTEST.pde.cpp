9ab23b6b551adca01a11612269ee63ad8f83ef3a
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-04-28 15:40:32 +0200

    formatowanie?

--- PROJECTS/MAPTEST/cppsrc/MAPTEST.pde.cpp
+++ PROJECTS/MAPTEST/cppsrc/MAPTEST.pde.cpp
@@ -1,4 +1,4 @@
-//Processing to C++ converter /data/wb/SCC/public/Processing2C/scripts/procesing2cpp.sh
+//Processing to C++ converter ../../scripts/procesing2cpp.sh
 //Source: MAPTEST.pde
 #include "processing_consts.hpp"
 #include "processing_templates.hpp"
@@ -89,5 +89,5 @@ void processing_window::setup()
   println("\nDefault print method:");
   println(mapString2Object);
 }
-///data/wb/SCC/public/Processing2C/scripts did it
+//../../scripts did it
 
