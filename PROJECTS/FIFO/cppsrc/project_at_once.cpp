d609c3e6f3916e408a32ddff24ba26b9d187dc18
Wojciech T. Borkowski <wborkowsk@gmail.com>
2023-05-06 08:44:05 +0200

    Zmiana_rozszerzeń_cd.

---
+++ PROJECTS/FIFO/cppsrc/project_at_once.cpp
@@ -0,0 +1,12 @@
+/*All sources in one file?*/
+#include "processing_consts.hpp"
+#include "processing_templates.hpp"
+#include "processing_library.hpp"
+#include "processing_inlines.hpp" //...is optional. Use when project is already compilable!
+#include "processing_window.hpp"
+#include "processing_string.hpp"  //Processing::String class
+using namespace Processing;
+#include "local.h"
+//==================================================================================
+const char* Processing::_PROGRAMNAME="FIFO";
+#include "FIFO.pde.hpp"
